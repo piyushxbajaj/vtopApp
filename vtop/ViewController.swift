14fd3cc7a9e8414f1490f8e15b5c1c11937ea34e
PiyushxBajaj <piyushpiyush41@gmail.com>
2019-07-30 13:50:49 +0530

    Files added

--- vtop/ViewController.swift
+++ vtop/ViewController.swift
@@ -6,7 +6,7 @@ class ViewController: NSViewController {
     @IBOutlet weak var load: WKWebView!
     
     //Variables
-    let url1 = URL(string: "https://apple.com")
+    let url1 = URL(string: "https://vtop.vit.ac.in/vtop/initialProcess")
     let customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.1 Safari/605.1.15"
  
     override func viewDidLoad() {
